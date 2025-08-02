b87ab04d7979dc3d5b0ee98ae7f2acf160146f46
William Fernandes Pereira <williamfernandesp@outlook.com>
2025-08-02 16:45:19 -0300

    Adicionado em LogMiddleware logica para tratamento de erros e logs estruturados

--- Fcg.Api/Middlewares/LogMiddleware.cs
+++ Fcg.Api/Middlewares/LogMiddleware.cs
@@ -1,27 +1,52 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Threading.Tasks;
 
 namespace Fcg.Api.Middlewares
 {
-    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
     public class LogMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<LogMiddleware> _logger;
 
-        public LogMiddleware(RequestDelegate next)
+        public LogMiddleware(RequestDelegate next, ILogger<LogMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
-        public Task Invoke(HttpContext httpContext)
+        public async Task Invoke(HttpContext httpContext)
         {
+            try
+            {
+                _logger.LogInformation("Request {method} {url} iniciada em {timestamp}",
+                    httpContext.Request.Method,
+                    httpContext.Request.Path,
+                    DateTime.UtcNow);
 
-            return _next(httpContext);
+                await _next(httpContext);
+
+                _logger.LogInformation("Response {statusCode} para {method} {url} em {timestamp}",
+                    httpContext.Response.StatusCode,
+                    httpContext.Request.Method,
+                    httpContext.Request.Path,
+                    DateTime.UtcNow);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro não tratado em {method} {url} em {timestamp}",
+                    httpContext.Request.Method,
+                    httpContext.Request.Path,
+                    DateTime.UtcNow);
+
+                httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+                await httpContext.Response.WriteAsync("{\"error\": \"Ocorreu um erro interno no servidor.\"}");
+            }
         }
     }
 
-    // Extension method used to add the middleware to the HTTP request pipeline.
     public static class LogMiddlewareExtensions
     {
         public static IApplicationBuilder UseLogMiddleware(this IApplicationBuilder builder)
